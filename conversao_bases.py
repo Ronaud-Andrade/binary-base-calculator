e55bf77b25e703789f1565e856872e47bd1e42da
Chapadin <ronaudandrade108@gmail.com>
2024-09-22 12:00:31 -0300

    Documenting the code
    Documenting the code and translating it into English

--- conversao_bases.py
+++ conversao_bases.py
@@ -1,21 +1,29 @@
-print("Esse programa é um conversor de bases")
-valor = input("Digite um valor: ")
-valor_characters = len(valor)
-base = str(input("Digite qual é a base que deseja converter: Ex.: Bin; Dec. (Sem acentos) "))
-base = base.lower()
+from os import system
+system('cls') #Clean the terminal
+
+print("PT-BR: Esse programa é um conversor de bases")
+print("EN: This program is a base converter")
+valor = input("Enter a value: ")
+valor_characters = len(valor) #Number of characters
+base = str(input("Enter the base you want to convert: Ex.: Bin; Dec. "))
+base = base.lower() #Transforming the string into lowercase
 
 valor_dec = 0
 if(base == "bin"):
     #casting
         valor = int(valor)
+        
+        #loop
         while(valor > 0 or valor > 1):
-            valor_inteiro = valor // 2 #4/2 = 2
-            valor_resto = valor % 2 #4mod2 = 0
+            valor_inteiro = valor // 2 #4//2 = 2
+            valor_resto = valor % 2 #4%2 = 0
             print(valor_resto)
             valor = valor_inteiro
+print("The displayed value is inverted. Example: 001 -> 100 (Binary) = 4 (Decimal)")
             
 if(base == "dec"):
     
+    #loop
     for i in range(valor_characters):
         if(valor[i] == 1):
             valor_convertido = i*2
