310f41839549ff138fc7b0098aa8b685987de46e
Chapadin <ronaudandrade108@gmail.com>
2024-09-29 19:28:24 -0300

    Adição de operação
    Adição da operação de conversão de binário para octal.

--- conversao_bases.py
+++ conversao_bases.py
@@ -3,28 +3,60 @@ system('cls') #Clean the terminal on windows
 
 print("PT-BR: Esse programa é um conversor de bases")
 print("EN: This program is a base converter")
+convert = input("Qual a base que vc quer converter? ex.: Bin; Dec; Oct; Hex.  ")
 valor = input("Enter a value: ")
 valor_characters = len(valor) #Number of characters
-base = str(input("Enter the base you want to convert: Ex.: Bin; Dec. "))
+base = str(input("Enter the base you want to convert: Ex.: Bin; Dec; Oct; Hex.  "))
 base = base.lower() #Transforming the string into lowercase
 array = []
-valor_dec = 0
 if(base == "bin"):
-    #casting
-        valor = int(valor)
-        #loop
-        while(valor > 0 or valor > 1):
-            valor_inteiro = valor // 2 #4/2 = 2
-            valor_resto = valor % 2 #4mod2 = 0
+    if("2" in valor):
+        print("Binario é somente '1' e '0'")
+    else:
+        #casting
+            valor = int(valor)
+            #loop
+            while(valor > 0 or valor > 1):
+                valor_inteiro = valor // 2 #4/2 = 2
+                valor_resto = valor % 2 #4mod2 = 0
+                #print(valor_resto, end="")
+                array.append(valor_resto)
+                #print(array)
+                valor = valor_inteiro
+        
+            for i in range(len(array)):
+                print(array[-1-i], end="")
+
+elif(base == "oct" and convert == "bin"):
+    if("2" in valor):
+        print("Binario é somente '1' e '0'")
+    else:
+        valor_dec = 0
+        #casting
+            #Convertendo para decimal
+        for i in range(valor_characters):
+            valor_dec += (int(valor[-1-i]))*(2**i)
+            
+        if(i == valor_characters - 1):
+            print(f"Converção para decimal: {valor_dec}", end="\n")
+        else:
+            print(f"Converção para decimal: {valor_dec}", end="")
+                
+        #Converter para octal       
+        valor_dec = int(valor_dec)
+        while((valor_dec) > 0 or (valor_dec) > 1):
+            valor_inteiro = valor_dec // 8 
+            valor_resto = valor_dec % 8
             #print(valor_resto, end="")
             array.append(valor_resto)
             #print(array)
-            valor = valor_inteiro
-    
+            valor_dec = valor_inteiro
+            
         for i in range(len(array)):
             print(array[-1-i], end="")
-                    
+
 elif(base == "dec"): 
+    valor_dec = 0
     #loop
     for i in range(valor_characters):
         valor_characters += -1
